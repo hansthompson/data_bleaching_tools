a1d44aa572eab62a5ad868925cfb3eae3e9e7e29
Charles Hans Thompson <hans.thompson1@gmail.com>
2014-08-21 15:23:06 -0800

    upper case the names

--- R/Bleach_Names.R
+++ R/Bleach_Names.R
@@ -3,5 +3,13 @@ Bleach_Names <- function(name) {
     name <- tolower(name)
     name <- str_trim(name)
     name <- str_replace_all(name, "[[:punct:]]", " ")
+    
+    simpleCap <- function(x) {
+        s <- strsplit(x, " ")[[1]]
+        paste(toupper(substring(s, 1,1)), substring(s, 2),
+              sep="", collapse=" ")
+    }
+    
+    sapply(name, simpleCap)
 }
 
